622e16e26f8ec768b04f9d9c5386aa9e2f38db53
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-08-07 22:59:35 +0700

    Added dummy scan fail/success

--- app/src/main/java/com/patrolsystemapp/ScanResultActivity.java
+++ app/src/main/java/com/patrolsystemapp/ScanResultActivity.java
@@ -1,17 +1,21 @@
 package com.patrolsystemapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.patrolsystemapp.Utils.Crypto;
 
 public class ScanResultActivity extends AppCompatActivity {
+    private static final String TAG="ScanResultActivity";
     private Context mContext;
-    private TextView txtToken;
-    private TextView txtEncrypt;
-
+    private LinearLayout loadingScan;
     private String token;
 
     @Override
@@ -24,10 +28,8 @@ public class ScanResultActivity extends AppCompatActivity {
 
     private void initWidgets() {
         mContext = ScanResultActivity.this;
-        txtToken = (TextView) findViewById(R.id.txtToken);
-        txtEncrypt = (TextView) findViewById(R.id.txtEncrypt);
-        token = getIntent().getStringExtra("TOKEN");
-        txtToken.setText(token);
+        token = getIntent().getStringExtra("SCANRES");
+        loadingScan = findViewById(R.id.loadingScan);
 
         String secret = "21072019_3_1_Cinthia Vicky Yolanda_1";
         String salt = "0123456789012345";
@@ -35,7 +37,22 @@ public class ScanResultActivity extends AppCompatActivity {
 
         Crypto crypto = new Crypto();
         String hasil = crypto.pbkdf2(secret, salt, iterations, 32);
-        txtEncrypt.setText(hasil);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "Hasil1: " + token);
+                Log.d(TAG, "Hasil2: " + hasil);
+                Log.d(TAG, "run: " + token.equals(hasil));
+
+                if(!token.equals(hasil)) {
+                    setContentView(R.layout.activity_scan_result_success);
+                } else {
+                    setContentView(R.layout.activity_scan_result_fail);
+                }
+            }
+        }, 3000);
     }
 
 }

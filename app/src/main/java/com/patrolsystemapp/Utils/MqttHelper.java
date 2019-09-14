a84da97d9733694520b0a400b97564eff80c29de
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-09-15 00:18:17 +0700

    tidy up code

--- app/src/main/java/com/patrolsystemapp/Utils/MqttHelper.java
+++ app/src/main/java/com/patrolsystemapp/Utils/MqttHelper.java
@@ -15,6 +15,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 

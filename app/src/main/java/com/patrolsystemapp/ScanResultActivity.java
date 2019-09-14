06f4f458a230ac10f8fba720b9e49f6ddc18d8f5
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-09-15 03:39:28 +0700

    WIP Scan result activity

--- app/src/main/java/com/patrolsystemapp/ScanResultActivity.java
+++ app/src/main/java/com/patrolsystemapp/ScanResultActivity.java
@@ -1,12 +1,15 @@
 package com.patrolsystemapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.gson.Gson;
@@ -18,66 +21,165 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.ConnectionSpec;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class ScanResultActivity extends AppCompatActivity {
     private static final String TAG = "ScanResultActivity";
-    private Context mContext;
-    private LinearLayout loadingScan;
-    private String scanResult;
-    //    Shared preferences
+    private String ipAddress;
     private SharedPreferences sharedPrefs;
+    private Context mContext;
+    private String scanResult;
+
+    private LinearLayout linearLayoutVerify;
+    private LinearLayout linearLayoutNoMatches;
+    private LinearLayout linearLayoutErrorVerify;
+
+    private Button btnToHome;
+    private Button btnReVerify;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_result);
         initWidgets();
+        verifyScanResult();
     }
 
     private void initWidgets() {
         mContext = ScanResultActivity.this;
-        scanResult = getIntent().getStringExtra("SCANRES");
-        loadingScan = findViewById(R.id.loadingScan);
-
         sharedPrefs = getSharedPreferences("patrol_app", Context.MODE_PRIVATE);
+        ipAddress = sharedPrefs.getString("ip_address", "");
+        scanResult = getIntent().getStringExtra("SCANRES");
 
-        String secret = "21072019_3_1_Cinthia Vicky Yolanda_1";
-        String salt = "0123456789012345";
-        int iterations = 100000;
+        linearLayoutVerify = findViewById(R.id.layoutVerify);
+        linearLayoutNoMatches = findViewById(R.id.layoutNoMatches);
+        linearLayoutErrorVerify = findViewById(R.id.layoutErrorVerify);
 
-        Crypto crypto = new Crypto();
-        String iteratedItem = crypto.pbkdf2(secret, salt, iterations, 32);
+        btnReVerify = findViewById(R.id.btnReVerify);
+        btnReVerify.setOnClickListener(v -> {
+            verifyScanResult();
+        });
 
-        String shifts = sharedPrefs.getString("shifts", "");
-        try {
-            JSONArray arr = new JSONArray(shifts);
-            Gson gson = new GsonBuilder().create();
+        btnToHome = findViewById(R.id.btnToHome);
+        btnToHome.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+    }
 
-            for(int i = 0; i < arr.length(); i++) {
-                JSONObject row = arr.getJSONObject(i);
-                Schedule schedule = gson.fromJson(row.toString(), Schedule.class);
+    private void verifyScanResult() {
+        runOnUiThread(() -> {
+            linearLayoutVerify.setVisibility(View.VISIBLE);
+            linearLayoutErrorVerify.setVisibility(View.GONE);
+            linearLayoutNoMatches.setVisibility(View.GONE);
+        });
 
-                String formatted = schedule.getDate() + "_" + schedule.getRoom();
-                System.out.println("huhuhuh " + formatted);
+        // use connectionSpecs so will work with regular HTTP
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.CLEARTEXT))
+                .build();
+
+        String url = "http://" + ipAddress;
+        Request request = new Request.Builder()
+                .url(url + "/api/guard/users/shifts/?token="
+                        + sharedPrefs.getString("token", ""))
+                .build();
+
+        Log.d(TAG, "requestSchedule: " + url + "/api/guard/users/shifts/?token="
+                + sharedPrefs.getString("token", ""));
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+
+                runOnUiThread(() -> {
+                    linearLayoutVerify.setVisibility(View.GONE);
+                    linearLayoutErrorVerify.setVisibility(View.VISIBLE);
+                });
             }
 
-        } catch (JSONException e) {
-            e.printStackTrace();
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonString = response.body().string();
+                try {
+                    JSONObject obj = new JSONObject(jsonString);
+                    System.out.println(obj.toString(2));
+                    boolean err = (Boolean) obj.get("error");
+                    if (!err) {
+                        confirmShift(obj.getJSONArray("data"));
+                    } else {
+                        throw new Exception("Error API!");
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> {
+                        linearLayoutVerify.setVisibility(View.GONE);
+                        linearLayoutErrorVerify.setVisibility(View.VISIBLE);
+                    });
+                }
+            }
+        });
+    }
+
+    public void confirmShift(JSONArray listShifts) throws JSONException {
+        String salt = sharedPrefs.getString("master_key", "0123456789012345");
+        int iterations = 100000;
+        Crypto crypto = new Crypto();
+
+        Log.d(TAG, "bestoo: " + salt);
+
+        boolean confirmed = false;
+        Schedule matchedShift;
+
+        Gson gson = new GsonBuilder().create();
+
+        if (listShifts.length() > 0) {
+            for (int i = 0; i < listShifts.length(); i++) {
+                JSONObject row = listShifts.getJSONObject(i);
+                Schedule schedule = gson.fromJson(row.toString(), Schedule.class);
+
+                String[] dateArr = schedule.getDate().split("-");
+                Collections.reverse(Arrays.asList(dateArr));
+                String formattedDate = TextUtils.join("", dateArr);
+
+                String secret =
+                        formattedDate + "_"
+                                + schedule.getRoom() + "_"
+                                + schedule.getId();
+
+                String shiftEncrypted = crypto.pbkdf2(secret, salt, iterations, 32);
+                if (scanResult.equals(shiftEncrypted)) {
+                    confirmed = true;
+                    matchedShift = schedule;
+                    break;
+                }
+            }
+
+            if (confirmed) {
+//                setContentView(R.layout.activity_scan_result_success);
+            } else {
+                runOnUiThread(() -> {
+                    linearLayoutVerify.setVisibility(View.GONE);
+                    linearLayoutNoMatches.setVisibility(View.VISIBLE);
+                });
+            }
+        } else {
+            runOnUiThread(() -> {
+                linearLayoutVerify.setVisibility(View.GONE);
+                linearLayoutNoMatches.setVisibility(View.VISIBLE);
+            });
         }
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Log.d(TAG, "Hasil1: " + scanResult);
-//                Log.d(TAG, "Hasil2: " + iteratedItem);
-//                Log.d(TAG, "run: " + scanResult.equals(iteratedItem));
-//
-//                if (!scanResult.equals(iteratedItem)) {
-//                    setContentView(R.layout.activity_scan_result_success);
-//                } else {
-//                    setContentView(R.layout.activity_scan_result_fail);
-//                }
-//            }
-//        }, 3000);
     }
 
 }

f888451261d1086428b8b3b68a6d2f70c15ee7e9
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2020-01-23 18:49:54 +0700

    WIP added image picker UI for confirm shift page

---
+++ app/src/main/java/com/patrolsystemapp/CustomLayout/SquareImageView.java
@@ -0,0 +1,29 @@
+package com.patrolsystemapp.CustomLayout;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+public class SquareImageView extends android.support.v7.widget.AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); // Snap to width
+    }
+}

ac72842005dde8253c0e4f02344705c496d11e63
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2020-04-15 22:35:16 +0700

    Printout derived keys on home page

--- app/src/main/java/com/patrolsystemapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/patrolsystemapp/Fragments/HomeFragment.java
@@ -29,6 +29,7 @@ import com.patrolsystemapp.Apis.NetworkClient;
 import com.patrolsystemapp.Apis.UploadApis;
 import com.patrolsystemapp.Model.Schedule;
 import com.patrolsystemapp.R;
+import com.patrolsystemapp.Utils.Crypto;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
@@ -36,6 +37,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -218,6 +224,56 @@ public class HomeFragment extends Fragment {
             });
         } else {
             mHandler.post(() -> linearLayoutNoShift.setVisibility(View.VISIBLE));
+        }
+
+        PrintSchedule printSchedule = new PrintSchedule(scheduleList);
+        Thread t = new Thread(printSchedule);
+        t.start();
+    }
+
+    private class PrintSchedule implements Runnable {
+        String salt = sharedPrefs.getString("master_key", "0123456789012345");
+        int iterations = 100000;
+        Crypto crypto = new Crypto();
+        private ArrayList<Schedule> listSchedule;
+
+        public PrintSchedule(ArrayList<Schedule> listSchedule) {
+            this.listSchedule = listSchedule;
+        }
+
+        @Override
+        public void run() {
+            ExecutorService executor = Executors.newFixedThreadPool(this.listSchedule.size());
+            CompletionService<String> completionService =
+                    new ExecutorCompletionService<>(executor);
+
+            for (int i = 0; i < listSchedule.size(); i++) {
+                Schedule schedule = listSchedule.get(i);
+                completionService.submit(() -> {
+                    String secret = schedule.getId();
+
+                    String shiftEncrypted = crypto.pbkdf2(secret, salt, iterations, 32);
+                    String processedListItem = shiftEncrypted.replaceAll("\\P{Print}", "");
+
+                    return processedListItem;
+                });
+            }
+
+            int received = 0;
+            while (received < listSchedule.size()) {
+                Future<String> resultFuture = null;
+                try {
+                    resultFuture = completionService.take();
+                    String derived = resultFuture.get();
+                    System.out.println(listSchedule.get(received).getId());
+                    System.out.println(listSchedule.get(received).getRoom());
+                    System.out.println("Key is : " + derived);
+                    System.out.println();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                received++;
+            }
         }
     }
 }
